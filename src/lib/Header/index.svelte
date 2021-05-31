412023bb8e861f3048bedd614506b6cf322d2bb3
Toinou941 <83634278+Toinou941@users.noreply.github.com>
2021-05-31 18:35:18 +0200

    Initial commit

---
+++ src/lib/Header/index.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+    import Modal from "./Modal.svelte";
+    let modal;
+</script>
+
+<header class="bg-green-400 text to-blue-500 p-4 flex justify-between items-center">
+	<div>
+		<a href="/">
+			<img class="h-20 w-30 inline-block border-ltext text-5xl text-white font-bold" src={logo} alt="healthkit" /> 
+		</a>
+	</div>
+	<div class="flex items-stretch">
+		<a href="/" class="inline-block p-2 text-blue-700 font-bold hover:text-white mr-2 text-3xl rounded-lg bg-white hover:bg-blue-500 hover:shadow-xl transition duration-300">Home</a>
+		<a href="/about" class="inline-block p-2 font-bold text-blue-700 hover:text-white mr-2 text-3xl rounded-lg bg-white hover:bg-blue-500 hover:shadow-xl transition duration-300">About</a>
+		<a href="/about" class="inline-block p-2 font-bold text-blue-700 hover:text-white text-3xl rounded-lg bg-white hover:bg-blue-500 hover:shadow-xl transition duration-300">Contact</a>
+
+	</div>
+	<div>
+		<button on:click={() => modal.show()} class="inline-block p-2 text-white hover:text-blue-400 mr-2 font-bold">Login</button>
+		<Modal bind:this={modal}>
+			<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+				<div class="mb-4">
+				  <label class="block text-grey-darker text-sm font-bold mb-2" for="username">
+					Username
+				  </label>
+				  <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username">
+				</div>
+				<div class="mb-6">
+				  <label class="block text-grey-darker text-sm font-bold mb-2" for="password">
+					Password
+				  </label>
+				  <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************">
+				</div>
+				<div class="flex items-center justify-between">
+				  <button class="bg-blue-700 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">
+					Sign In
+				  </button>
+				  <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
+					Forgot Password?
+				  </a>
+				</div>
+			</div>
+		  <button on:click={() => modal.hide()}>Close</button>
+		</Modal>
+		<a href="/register" class="inline-block py-2 px-4 text-blue-700 bg-white hover:bg-blue-500 font-bold hover:text-white rounded transition ease-in duration-150">Sign Up</a>
+	</div>
+
+	
+</header>
+  
---
+++ src/routes/index.svelte
@@ -0,0 +1,48 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section class="relative flex items-center justify-center h-screen overflow-hidden">
+		<div
+		href="#video"class="relative z-30 p-5 text-2xl text-blue-500 bg-green-500 bg-opacity-80 rounded-xl"
+		>
+		  Welcome to Healthcare + !
+		</div>
+		<video
+		  autoplay
+		  loop
+		  muted
+		  class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+		>
+		  <source
+			src="src\routes\img\vecteezy_green-particles-on-this-medical-grid-with-a-hexagon-pattern-background-loop_2018897.mp4"
+			type="video/mp4"
+		  />
+		  Your browser does not support the video tag.
+		</video>
+</section>
+	<!-- big hero landing page-->
+<section class="min-h-screen bg-blue-100 p-32 text-center" style="background-image: url('https://images.unsplash.com/photo-1472983131032-23a243d662eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80'); background-size: cover">
+
+		<h1 class="text-5xl font-bold mb-2 bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-green-500">Learn more about our mission</h1>
+	  
+	  	<!-- svelte-ignore a11y-media-has-caption -->
+		<video id="video" controls class="pb-8 max-w left-6 border-collapse"  src="video.mp4"></video>
+		<div class="space-x-2">
+		  <a href="#more" class="py-3 px-8 pt-4 text-blue-500 hover:text-blue-400 bg-white hover:bg-gray-200 mr-2  rounded-lg hover:shadow-xl transition duration-300">Learn More</a>
+		  <a href="/register" class="py-3 px-8 pt-4 text-blue-500 hover:text-blue-400 bg-white hover:bg-gray-200 mr-2  rounded-lg hover:shadow-xl transition duration-300">Sign up</a>
+		</div>
+	  
+</section>
+<section id="more" class="min-h-screen bg-blue-100 p-32 text-center" style="background-image: url('https://images.unsplash.com/photo-1603715749723-240c88df18b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'); background-size: cover">
+
+	<h1 class="text-5xl text-white"> description of our mission, whats the purpose and so on...</h1>
+  
+</section>
+
+  
+  
